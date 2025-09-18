9038c76a8aa1ad7d929d587244aaa636e58322e9
affworldt <affworldtechnologies@gmail.com>
2025-09-18 19:06:13 +0400

    dd

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,20 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatCurrency(amount: number, currency: string = 'AED'): string {
+  return new Intl.NumberFormat('en-AE', {
+    style: 'currency',
+    currency: currency,
+    minimumFractionDigits: 0,
+    maximumFractionDigits: 2,
+  }).format(amount)
+}
+
+export function formatPrice(price: number): string {
+  return `AED ${price.toFixed(2)}`
+}
+
+export function formatNumber(num: number): string {
+  return new Intl.NumberFormat('en-AE').format(num)
+}
